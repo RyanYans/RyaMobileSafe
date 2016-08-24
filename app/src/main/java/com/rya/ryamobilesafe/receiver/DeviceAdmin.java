4c653d82c6240a6b6b57f0c7573038760bbf5108
RyanYans <ryanyans32@gmail.com>
2016-08-24 23:50:38 +0800

    night 定位信息、远程锁屏、擦除数据、报警铃 day05

---
+++ app/src/main/java/com/rya/ryamobilesafe/receiver/DeviceAdmin.java
@@ -0,0 +1,17 @@
+package com.rya.ryamobilesafe.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Rya32 on 广东石油化工学院.
+ * Version 1.0
+ */
+public class DeviceAdmin extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+    }
+}
--- app/src/main/java/com/rya/ryamobilesafe/receiver/SmsReceiver.java
+++ app/src/main/java/com/rya/ryamobilesafe/receiver/SmsReceiver.java
@@ -1,31 +1,35 @@
 package com.rya.ryamobilesafe.receiver;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 
 import com.rya.ryamobilesafe.R;
 import com.rya.ryamobilesafe.service.LocationService;
 import com.rya.ryamobilesafe.utils.ConstantValues;
 import com.rya.ryamobilesafe.utils.SPUtil;
+import com.rya.ryamobilesafe.utils.ToastUtil;
 
 /**
  * Created by Rya32 on 广东石油化工学院.
  * Version 1.0
  */
 public class SmsReceiver extends BroadcastReceiver {
+
+
+    private DevicePolicyManager mDPM;
+    private ComponentName mComponentName;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         boolean safe_isopen = SPUtil.getBoolean(context, ConstantValues.SAFE_ISOPEN, false);
+
+        mComponentName = new ComponentName(context, DeviceAdmin.class);
+        mDPM = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
         if (safe_isopen) {
             Object[] pdus = (Object[]) intent.getExtras().get("pdus");
             for (Object obj : pdus) {
@@ -38,6 +42,19 @@ public class SmsReceiver extends BroadcastReceiver {
                 } else if (messageBody.contains("#*location*#")) {
                     Intent intentLocation = new Intent(context, LocationService.class);
                     context.startService(intentLocation);
+                } else if (messageBody.contains("#*wipedata*#")) {
+                    if (mDPM.isAdminActive(mComponentName)) {
+                        mDPM.wipeData(0);
+                    } else {
+                        ToastUtil.show(context, "请到设置 ->安全 ->设备管理器中勾选服务！");
+                    }
+                } else if (messageBody.contains("#*lockscreen*#")) {
+                    if (mDPM.isAdminActive(mComponentName)) {
+                        mDPM.lockNow();
+                        mDPM.resetPassword("123456", 0);
+                    } else {
+                        ToastUtil.show(context, "请到 设置 ->安全 ->设备管理器 中勾选服务！");
+                    }
                 }
             }
         }
